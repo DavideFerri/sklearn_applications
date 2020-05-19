7058238502bed32606210141f05bc4c80ac3bd60
DAVIDE <davide.ferri.94@gmail.com>
2020-05-19 20:08:41 +0200

    train test split added in main

--- BivariateClassification_MLexample/code/Main.py
+++ BivariateClassification_MLexample/code/Main.py
@@ -9,6 +9,7 @@ Created on Tue May 19 19:24:35 2020
 import numpy as np 
 import pandas as pd
 from SynthetData_Gen import synthetic_data_generator
+from sklearn.model_selection import train_test_split
 #import matplotlib.pyplot as plt
 
 # initialize the logger
@@ -23,8 +24,16 @@ size = 1000
 dgp = "quadratic"
 # set the parameters
 true_parameters = {"alpha" : 2, "beta1" : 2, "beta2" : 4} 
+# set the test size as a proportion 
+test_size = 0.2
 
 # -------------------- generata synthetic data ---------------------- # 
 
 # produce synthetic data 
 data = synthetic_data_generator(size = size,dgp = dgp,parameters = true_parameters,plots = True) 
+
+# -------------------- preprocessing data --------------------------- # 
+
+# get the splin between train and test data
+X_train, X_test, y_train, y_test = train_test_split(data[:,1:],data[:,0],test_size = test_size)
+log.info("X train shape %s",X_train.shape) ; log.info("X test shape %s",X_test.shape)
---
+++ BivariateClassification_MLexample/code/models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 19:43:52 2020
+
+@author: davideferri
+"""
+
+import numpy as np 
+import pandas as pd
+import scipy.stats as ss 
+import logging
+import math
+import matplotlib.pyplot as plt
+
+# ----------------------- utilities ------------------------------- # 
+
+
+
+
+
+# ---------------------- configuration --------------------------- # 
+
+# initialize the logger
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,format='%(name)s - %(levelname)s - %(message)s')
+
